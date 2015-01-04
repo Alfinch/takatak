fe473eb19aecee2880c8cf84ce2e23aa58e154c0
Alfinch <alfiewoodland@gmail.com>
2015-01-04 22:17:23 +0000

    Started work on game logic

---
+++ takatak.graphics.js
@@ -0,0 +1,18 @@
+/*
+ * TAKATAK
+ * A probability-based shoot-em-up typing game
+ * 
+ * (c) Alfie Woodland 2015
+ */
+
+'use strict';
+
+var game;
+
+game = (function (game) {
+
+
+
+  return game;
+
+}(game || {}));
